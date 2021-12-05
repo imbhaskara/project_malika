f16e896bbede789f6d26da2c5f9f818f0de7c8f4
Imam Bhaskara <63944529+imbhaskara@users.noreply.github.com>
2021-12-05 20:19:09 +0700

    Add files via upload

---
+++ delete_plan.py
@@ -0,0 +1,75 @@
+import streamlit as st
+import pandas as pd
+from conn_db import filter_data, delete_data
+import datetime as dt
+ 
+def delete_plan():
+    st.subheader('Hapus Data Individual Plan 2022')
+    nik = st.number_input('Masukkan data NIK Karyawan')
+    data = filter_data(nik)
+    if data:
+        ambition = data[0][1]
+        strength = data[0][2]
+        weakness = data[0][3]
+        competencies_1 = data[0][4]
+        dev_type_1 = data[0][5]
+        dev_desc_1 = data[0][6]
+        dev_date_1 = data[0][7]
+        competencies_2 = data[0][8]
+        dev_type_2 = data[0][9]
+        dev_desc_2 = data[0][10]
+        dev_date_2 = data[0][11]
+        competencies_3 = data[0][12]
+        dev_type_3 = data[0][13]
+        dev_desc_3 = data[0][14]
+        dev_date_3 = data[0][15]
+        competencies_4 = data[0][16]
+        dev_type_4 = data[0][16]
+        dev_desc_4 = data[0][17]
+        dev_date_4 = data[0][18]
+        competencies_5 = data[0][19]
+        dev_type_5 = data[0][20]
+        dev_desc_5 = data[0][21]
+        dev_date_5 = data[0][22]
+        recommendation = data[0][23]
+
+        new_ambition = st.text_area('Career Ambition in 2022', value=ambition)
+        st.caption('Ceritakan hal-hal yang ingin Anda capai terkait Karir dan Pengembangan di tahun 2022')
+        strength_cols, weakness_cols = st.columns((2,2))
+        new_strength = strength_cols.text_area('Employee Strength', strength)
+        strength_cols.caption('Ceritakan tentang kompetensi yang menjadi kekuatan dari karyawan Anda')
+        new_weakness = weakness_cols.text_area('Employee Weakness', weakness)
+        weakness_cols.caption('Ceritakan tentang kompetensi yang masih kurang dari karyawan Anda')
+        st.write('Jenis Development yang ingin dilakukan')
+        st.caption('Ceritakan maksimal 3 Jenis Development yang ingin Anda ambil di tahun 2022 untuk meningkatkan kompetensi Anda')
+        activity, jenis_dev, deskripsi_dev, tgl_dev = st.columns((2,2,2,1))
+        new_competencies_1 = activity.text_input('Nama Kompetensi', competencies_1, key=1)
+        new_dev_type_1 = jenis_dev.selectbox('Jenis Development', ['Development 70', 'Development 20', 'Development 10'], key=1)
+        new_dev_desc_1 = deskripsi_dev.text_area('Deskripsi Development yang akan dilakukan', dev_desc_1, key=1)
+        new_dev_date_1 = tgl_dev.date_input('Deadline Realisasi',value=dt.datetime.strptime(dev_date_1, '%Y-%m-%d'), key=1)
+        activity2, jenis_dev2, deskripsi_dev2, tgl_dev2 = st.columns((2,2,2,1))
+        new_competencies_2 = activity2.text_input('Nama Kompetensi', competencies_2, key=2)
+        new_dev_type_2 = jenis_dev2.selectbox('Jenis Development', ['Development 70', 'Development 20', 'Development 10'], key=2)
+        new_dev_desc_2 = deskripsi_dev2.text_area('Deskripsi Development yang akan dilakukan', dev_desc_2, key=2)
+        new_dev_date_2 = tgl_dev2.date_input('Deadline Realisasi',value=dt.datetime.strptime(dev_date_2, '%Y-%m-%d'), key=2)
+        activity3, jenis_dev3, deskripsi_dev3, tgl_dev3 = st.columns((2,2,2,1))
+        new_competencies_3 = activity3.text_input('Nama Kompetensi', competencies_3, key=3)
+        new_dev_type_3 = jenis_dev3.selectbox('Jenis Development', ['Development 70', 'Development 20', 'Development 10'], key=3)
+        new_dev_desc_3 = deskripsi_dev3.text_area('Deskripsi Development yang akan dilakukan', dev_desc_3, key=3)
+        new_dev_date_3 = tgl_dev3.date_input('Deadline Realisasi',value=dt.datetime.strptime(dev_date_3, '%Y-%m-%d'), key=3)
+        activity4, jenis_dev4, deskripsi_dev4, tgl_dev4 = st.columns((2,2,2,1))
+        new_competencies_4 = activity4.text_input('Nama Kompetensi', competencies_4, key=4)
+        new_dev_type_4 = jenis_dev4.selectbox('Jenis Development', ['Development 70', 'Development 20', 'Development 10'], key=4)
+        new_dev_desc_4 = deskripsi_dev4.text_area('Deskripsi Development yang akan dilakukan', dev_desc_4, key=4)
+        new_dev_date_4 = tgl_dev4.date_input('Deadline Realisasi',value=dt.datetime.strptime(dev_date_4, '%Y-%m-%d'), key=4)
+        activity5, jenis_dev5, deskripsi_dev5, tgl_dev5 = st.columns((2,2,2,1))
+        new_competencies_5 = activity5.text_input('Nama Kompetensi', competencies_5, key=5)
+        new_dev_type_5 = jenis_dev5.selectbox('Jenis Development', ['Development 70', 'Development 20', 'Development 10'], key=5)
+        new_dev_desc_5 = deskripsi_dev5.text_area('Deskripsi Development yang akan dilakukan', dev_desc_5, key=5)
+        new_dev_date_5 = tgl_dev5.date_input('Deadline Realisasi',value=dt.datetime.strptime(dev_date_5, '%Y-%m-%d'), key=5)
+        new_recommendation = st.text_area('Catatan / Rekomendasi', recommendation)
+        st.caption('Tuliskan kesimpulan akhir, catatan, dan rekomendasi dari hasil diskusi dengan karyawan Anda ')
+        delete = st.button('Delete Data')
+        if delete:
+           delete_data(nik)
+           st.success(f'Data {nik} telah berhasil dihapus')
---
+++ development_plan.py
@@ -0,0 +1,35 @@
+from delete_plan import delete_plan
+from edit_plan import edit_plan
+import streamlit as st
+import pandas as pd
+from new_plan import create_new_plan
+from conn_db import view_all_data, get_table_download_link
+
+today = pd.Timestamp('today')
+
+#Page Configuration
+st.set_page_config(page_title='Malika', layout='wide', initial_sidebar_state='collapsed')
+
+menu = ['Home','Create New Development Plan', 'View Development Database', 'Edit or Manage Development Plan', 'Delete Development Plan']
+choice = st.sidebar.selectbox('Pilih menu yang ingin dikunjungi', menu)
+
+def development_plan():
+    if choice=='Home':
+        st.title(f'Individual Development Plan Tahun 2022')
+        st.write(f'Tanggal akses anda: {today:%d %B %Y}')
+    elif choice=='Create New Development Plan':
+        create_new_plan()
+    elif choice=='Edit or Manage Development Plan':
+        edit_plan()
+    elif choice=='Delete Development Plan':
+        delete_plan()
+    elif choice=='View Development Database':
+        st.subheader('Rekapitulasi Data Development Plan 2022')
+        output_name=f'Data_IDP_2022_{today:%d_%B_%Y}.xlsx'
+        result = view_all_data()
+        data = pd.DataFrame(result, columns=['NIK', 'Career Ambition', 'Employee Strength', 'Employee Weakness', 'Kompetensi 1', 'Jenis Development 1', 'Deskripsi Development 1', 'Deadline Development 1', 'Kompetensi 2', 'Jenis Development 2', 'Deskripsi Development 2', 'Deadline Development 2', 'Kompetensi 3', 'Jenis Development 3', 'Deskripsi Development 3', 'Deadline Development 3', 'Kompetensi 4', 'Jenis Development 4', 'Deskripsi Development 4', 'Deadline Development 4', 'Kompetensi 5', 'Jenis Development 5', 'Deskripsi Development 5', 'Deadline Development 5', 'Rekomendasi / Catatan'])
+        st.write(data)
+        st.markdown(get_table_download_link(data, output_file=output_name), unsafe_allow_html=True)
+
+if __name__ == '__main__':
+    development_plan()
